232afa5ae8b2e7a9e38dc8f4d92e9ac6f0b3cf2f
xiaohuojian-xys <18253718613@163.com>
2022-06-30 10:02:51 +0800

    read file

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,20 +1,27 @@
 package com.example.demo.controller;
 
 import com.example.demo.util.SystemUtil;
-import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@PropertySource(value = {"file:${config.path}"}, encoding="utf-8")
 public class DemoController {
 
-    public static Map<String, String> usage = new HashMap<String, String>();
+    @Value("${hostname}")
+    private String hostname;
+    @Value("${cpu}")
+    private String cpu;
+    @Value("${mem}")
+    private String mem;
+    @Value("${disk}")
+    private String disk;
+    private static Map<String, String> usage = new HashMap<String, String>();
 
     @GetMapping("/info")
     public Map<String, String> info(){
@@ -26,18 +33,29 @@ public class DemoController {
         return map;
     }
 
-    @GetMapping("/getinfo")
+    @GetMapping("/getInfo")
     public Map<String, String> getInfo(){
         return usage;
     }
 
-    @PostMapping("/setSysInfo")
-    public void setSysInfo(@RequestBody Map<String, String> map){
-        String cpu = map.get("cpu");
-        String mem = map.get("mem");
-        String disk = map.get("disk");
+//    @PostMapping("/setSysInfo")
+//    public void setSysInfo(@RequestBody Map<String, String> map){
+//        String hostname = map.get("hostname");
+//        String cpu = map.get("cpu");
+//        String mem = map.get("mem");
+//        String disk = map.get("disk");
+//        usage.put("hostname", hostname);
+//        usage.put("cpu", cpu);
+//        usage.put("mem", mem);
+//        usage.put("disk", disk);
+//    }
+
+    @GetMapping("/getSysInfo")
+    public Map<String, String> getSysInfo(){
+        usage.put("hostname", hostname);
         usage.put("cpu", cpu);
         usage.put("mem", mem);
         usage.put("disk", disk);
+        return usage;
     }
 }
